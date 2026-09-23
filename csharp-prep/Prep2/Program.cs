61bfb1fd98cfec982decbf111d6690c08b886f09
Keyan Dixon <keyandixon@gmail.com>
2026-09-22 20:30:32 -0600

    Moving Grade Prove Project to Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+ static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+    string grade;
+    Console.WriteLine("What grade percentage did you get?");    
+    grade = Console.ReadLine();
+    int gradeNumber = int.Parse(grade);
+    string finalScore;
+    string passFail;
+
+
+    if (gradeNumber >= 90)
+        {
+         finalScore = "A";
+         passFail = "Pass";   
+        }
+    else if (gradeNumber >= 80)
+        {
+         finalScore = "B";
+         passFail = "Pass";
+        }
+    else if (gradeNumber >= 70)
+        {
+         finalScore = "C";
+         passFail = "Pass";
+        }
+    else if (gradeNumber >= 60)
+        {
+         finalScore = "D"; 
+         passFail = "Fail";  
+        }    
+    else
+        {
+         finalScore = "F"; 
+         passFail = "Fail";  
+        }
+
+    Console.WriteLine(finalScore);
+    Console.WriteLine(passFail);            
     }
 }
